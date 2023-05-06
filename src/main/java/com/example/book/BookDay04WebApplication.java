ddef54bed01f34c0c86162bc09bf59b4f40225bb
李崇 <qazmlp2426@qq.com>
2023-05-06 22:28:31 +0800

    one

---
+++ src/main/java/com/example/book/BookDay04WebApplication.java
@@ -0,0 +1,13 @@
+package com.example.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDay04WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDay04WebApplication.class, args);
+	}
+
+}
